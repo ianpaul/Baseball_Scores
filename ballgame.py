5a5c6e0ba0efc42226beecf583e35f40434ea729
Ian Paul <ian@ianpaul.net>
2022-05-14 11:02:12 +0000

    I've moved all three scripts into one larger script.

--- ballgame.py
+++ ballgame.py
@@ -1,34 +1,62 @@
 import statsapi
+from datetime import date, timedelta
 
-txt = input("Name your team: ")
-team_name = txt
+txt1 = input("What would you like to see: \n(1) Latest game results \n(2) Schedule information \n(3) MLB standings \n")
+data_req = txt1
 
-def get_team_name(team_name):
-	team = statsapi.lookup_team(team_name)
-	return str(team[0]['id'])
+if data_req == "3":
+    txt2 = input("Do you want the AL, NL, or both?")
+    if txt2.casefold() == "al":
+        print(statsapi.standings(leagueId=103))
+    elif txt2.casefold() == "nl":
+        print(statsapi.standings(leagueId=104))
+    else:
+        print(statsapi.standings(leagueId=103))
+        print(statsapi.standings(leagueId=104))
 
-def recent_game():
-	team_number = get_team_name(team_name)
-	game = statsapi.last_game(team_number)
-	return game
+elif data_req == "1":
+    
+    txt = input("Name your team: ")
+    team_name = txt
+    def get_team_name(team_name):
+	    team = statsapi.lookup_team(team_name)
+	    return str(team[0]['id'])
 
-def show_boxscore():
-	game = recent_game()
-	box_score = statsapi.boxscore(game, battingBox=True, battingInfo=True, fieldingInfo=True, pitchingBox=True, gameInfo=True, timecode=None)
-	print(box_score)
+    def recent_game():
+	    team_number = get_team_name(team_name)
+	    game = statsapi.last_game(team_number)
+	    return game
 
-def show_linescore():
-	game = recent_game()
-	line_score = statsapi.linescore(game, timecode=None)
-	print(line_score)
+    def show_boxscore():
+	    game = recent_game()
+	    box_score = statsapi.boxscore(game, battingBox=True, battingInfo=True, fieldingInfo=True, pitchingBox=True, gameInfo=True, timecode=None)
+	    print(box_score)
 
-
-second_check = input("Do you want to see the latest full boxscore (1) or linescore (2)?")
-
-if second_check == "1":
-	show_boxscore()
-else:
-	show_linescore()
+    def show_linescore():
+	    game = recent_game()
+	    line_score = statsapi.linescore(game, timecode=None)
+	    print(line_score)
 
 
+    second_check = input("Do you want to see the latest full boxscore (1) or linescore (2)?")
 
+    if second_check == "1":
+	    show_boxscore()
+    else:
+	    show_linescore()
+
+elif data_req == "2":
+
+    txt = input("Name your team: ")
+    team_name = txt
+    def get_team_name(team_name):
+        team = statsapi.lookup_team(team_name)
+        return str(team[0]['id'])
+     
+    def sched():
+        crew = get_team_name(team_name)
+        agenda = statsapi.schedule(start_date=date.today(),end_date=date.today() + timedelta(days=2),team=crew)
+        for x in agenda:
+            print(x['summary'])
+
+    sched()
